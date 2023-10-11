ec72985651d8186ec1724ceea3c23391c5ff0839
Madina-88 <ergasevamalika066.gmail.com>
2023-10-11 18:07:03 +0300

    asdasd

---
+++ src/Main.java
@@ -0,0 +1,53 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+       /* System.out.println("У тебя есть четыре направления куда ты пойдешь?");
+        Scanner scanner = new Scanner(System.in);
+        String str = scanner.nextLine();
+        switch (str){
+            case "Пойду налево":
+                System.out.println("Ты пойдешь в салон красоты");
+                break;
+            case "Пойду направо":
+                System.out.println("Ты пойдешь в школу");
+                break;
+            case "Пойду вперед":
+                System.out.println("Ты пойдешь в магазин");
+                break;
+            case "Пойду назад":
+                System.out.println("Ты пойдешь домой");
+                break;*/
+
+        /*System.out.println("Введите число");
+        Scanner scanner = new Scanner(System.in);
+        int num1 = scanner.nextInt();
+        if (num1%2==0) {
+            System.out.println("Вы ввели четное число");
+        }else {
+            System.out.println("Вы ввели нечетное число");
+        }*/
+
+        /*System.out.println("Я загадал число от 1 до 5 угадай ее");
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
+        int num1 = random.nextInt(1,5);
+        int num2 = scanner.nextInt();
+        if (num1==num2){
+            System.out.println("я загадал "+ num1);
+        }else {
+            System.out.println("поздравляю загадал "+num2);
+        }*/
+
+
+        /*System.out.println("Загадай число от 1 до 20:");
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
+        int a = random.nextInt(1,20);
+        int b = 5;
+        int c = 3;
+        int i = 15;*/
+
+    }
+}
